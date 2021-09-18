cc5a55776c9815fc50651865f2d967fe258905ba
Tristan Elliott <>
2021-09-18 20:19:53 -0300

    adding editor

--- src/Components/Editor/index.js
+++ src/Components/Editor/index.js
@@ -14,42 +14,40 @@ c = \\pm\\sqrt{a^2 + b^2}
 
 
 const Editor =()=>{
-    const [data,setData] = useState(mdKaTeX)
+    const [data,setData] = useState("")
 
     return(
         <div>
-             <MDEditor
-      value={data}
-      onChange={setData}
-      previewOptions={{
-        components: {
-          code: ({ inline, children, className, ...props }) => {
-            const txt = children[0] || '';
-            if (inline) {
-              if (typeof txt === 'string' && /^\$\$(.*)\$\$/.test(txt)) {
-                const html = katex.renderToString(txt.replace(/^\$\$(.*)\$\$/, '$1'), {
-                  throwOnError: false,
-                });
-                return <code dangerouslySetInnerHTML={{ __html: html }} />;
-              }
-              return <code>{txt}</code>;
-            }
-            if (
-              typeof txt === 'string' &&
-              typeof className === 'string' &&
-              /^language-katex/.test(className.toLocaleLowerCase())
-            ) {
-              const html = katex.renderToString(txt, {
-                throwOnError: false,
-              });
-              return <code dangerouslySetInnerHTML={{ __html: html }} />;
-            }
-            return <code className={String(className)}>{txt}</code>;
-          },
-        },
-      }}
-    />
-         
+              <MDEditor value={data} onChange={setData}
+              previewOptions={{
+                components: {
+                  code: ({ inline, children, className, ...props }) => {
+                    const txt = children[0] || '';
+                    if (inline) {
+                      if (typeof txt === 'string' && /^\$\$(.*)\$\$/.test(txt)) {
+                        const html = katex.renderToString(txt.replace(/^\$\$(.*)\$\$/, '$1'), {
+                          throwOnError: false,
+                        });
+                        return <code dangerouslySetInnerHTML={{ __html: html }} />;
+                      }
+                      return <code>{txt}</code>;
+                    }
+                    if (
+                      typeof txt === 'string' &&
+                      typeof className === 'string' &&
+                      /^language-katex/.test(className.toLocaleLowerCase())
+                    ) {
+                      const html = katex.renderToString(txt, {
+                        throwOnError: false,
+                      });
+                      return <code dangerouslySetInnerHTML={{ __html: html }} />;
+                    }
+                    return <code className={String(className)}>{txt}</code>;
+                  },
+                },
+              }}
+            />
+              />   
         </div>
     )
 }
