e34932f93fd03bc5b52d38651b17e672d4594509
Tristan Elliott <>
2021-09-18 15:59:06 -0300

    adding the editor

--- src/Components/Editor/index.js
+++ src/Components/Editor/index.js
@@ -1,11 +1,55 @@
-import React from 'react';
+import React,{useState} from 'react';
+import MDEditor from '@uiw/react-md-editor';
+import katex from 'katex';
+import 'katex/dist/katex.css';
+
+const mdKaTeX = `This is to display the 
+\`\$\$\c = \\pm\\sqrt{a^2 + b^2}\$\$\`
+ in one line
+
+\`\`\`KaTeX
+c = \\pm\\sqrt{a^2 + b^2}
+\`\`\`
+`;
 
 
 const Editor =()=>{
+    const [data,setData] = useState(mdKaTeX)
 
     return(
         <div>
-            <h1>THIS IS THE EDITOR</h1>
+             <MDEditor
+      value={data}
+      onChange={setData}
+      previewOptions={{
+        components: {
+          code: ({ inline, children, className, ...props }) => {
+            const txt = children[0] || '';
+            if (inline) {
+              if (typeof txt === 'string' && /^\$\$(.*)\$\$/.test(txt)) {
+                const html = katex.renderToString(txt.replace(/^\$\$(.*)\$\$/, '$1'), {
+                  throwOnError: false,
+                });
+                return <code dangerouslySetInnerHTML={{ __html: html }} />;
+              }
+              return <code>{txt}</code>;
+            }
+            if (
+              typeof txt === 'string' &&
+              typeof className === 'string' &&
+              /^language-katex/.test(className.toLocaleLowerCase())
+            ) {
+              const html = katex.renderToString(txt, {
+                throwOnError: false,
+              });
+              return <code dangerouslySetInnerHTML={{ __html: html }} />;
+            }
+            return <code className={String(className)}>{txt}</code>;
+          },
+        },
+      }}
+    />
+         
         </div>
     )
 }
